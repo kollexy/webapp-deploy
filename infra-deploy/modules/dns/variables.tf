076dfcb92e8e08b9b00687bf3e520945c9684db6
kollexy <59570875+kollexy@users.noreply.github.com>
2024-06-25 19:09:53 +0100

    Add files via upload

---
+++ infra-deploy/modules/dns/variables.tf
@@ -0,0 +1,21 @@
+
+variable "resource_group_location" {
+  type = string
+  
+}
+variable "resource_group_name" {
+  type = string
+  
+}
+
+variable "vnetid" {
+  type = string
+}
+
+variable "private_ip_address" {
+  
+}
+
+variable "appsvc_name" {
+  
+}

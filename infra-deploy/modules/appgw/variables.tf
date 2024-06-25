d3352a651772fcb8060dcf3aedbc261e27f57c3e
kollexy <59570875+kollexy@users.noreply.github.com>
2024-06-25 19:07:44 +0100

    Add files via upload

---
+++ infra-deploy/modules/appgw/variables.tf
@@ -0,0 +1,17 @@
+variable "resource_group_location" {
+  type = string
+  
+  
+}
+variable "resource_group_name" {
+  type = string
+  
+  
+}
+
+variable "agw_subnet_id" {
+  type = string
+}
+variable "app_service_hostname" {
+  
+}

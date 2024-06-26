4475bd9a64700ea741c46ed8e4c821da82e53427
kollexy <59570875+kollexy@users.noreply.github.com>
2024-06-26 11:17:13 +0100

    update

--- infra-deploy/appservice.tf
+++ infra-deploy/appservice.tf
@@ -5,52 +5,21 @@ resource "azurerm_resource_group" "rg" {
 
 # App Service Plan
 resource "azurerm_service_plan" "asp" {
-  name                = "${var.resource_group_name}-asp"
-  location            = var.resource_group_location
-  resource_group_name = var.resource_group_name
+  name                = "${azurerm_resource_group.rg.name}-asp"
+  location            = azurerm_resource_group.rg.location  
+  resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
   sku_name            = "B1"
 }
 
 # App Service
 resource "azurerm_linux_web_app" "apps" {
-  name                  = "${var.resource_group_name}-appservice"
-  location              = var.resource_group_location
-  resource_group_name   = var.resource_group_name
+  name                  = "${azurerm_resource_group.rg.name}-appservice"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
   service_plan_id       = azurerm_service_plan.asp.id
   https_only            = true
   site_config { 
     minimum_tls_version = "1.2"
   }
 }
-
-# resource "azurerm_app_service_source_control" "sourcecontrol" {
-#   app_id             = azurerm_linux_web_app.apps.id
-#   repo_url           = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
-#   branch             = "master"
-#   use_manual_integration = true
-#   use_mercurial      = false
-# }
-
-# Private Endpoint for App Service
-# resource "azurerm_private_endpoint" "pep" {
-#   name                = "${var.resource_group_name}-private-endpoint"
-#   location            = var.resource_group_location
-#   resource_group_name = var.resource_group_name
-#   subnet_id           = var.subnet_id   ####azurerm_subnet.appservice_subnet.id
-
-#   private_service_connection {
-#     name                           = "apps-privateserviceconnection"
-#     private_connection_resource_id = azurerm_linux_web_app.apps.id
-#     subresource_names              = ["sites"]
-#     is_manual_connection           = false
-#   }
-# }
-
-# ##binding
-
-# resource "azurerm_app_service_custom_hostname_binding" "appsvc_binding" {
-#   hostname            = "test.devopslabs.uk"
-#   app_service_name    = azurerm_app_service.apps.name
-#   resource_group_name = var.resource_group_name
-# }
